942fd383560b0d90f6e626e3ad0ee106c48e7605
nedasoltanih <neda.sltn_h@yahoo.com>
2025-11-17 12:41:43 +0330

    Homework #1

--- reminder/todo/models.py
+++ reminder/todo/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateField()          # تاریخ سررسید
     due_time = models.TimeField()          # ساعت سررسید
     done = models.BooleanField(default=False)  # انجام شده یا نه
+
+    def __str__(self):
+        return self.title
+
+    @staticmethod
+    def due_date_passed():
+        return Task.objects.filter(due_date__lt=timezone.now(), done=False)
+
+    def not_done(self):
+        if self.due_date < timezone.now().date() and not self.done:
+            return True
+        return False
--- reminder/todo/views.py
+++ reminder/todo/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Task
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+def task_detail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'details.html', {'task': task})
