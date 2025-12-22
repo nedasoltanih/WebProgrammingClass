aa45d78b11168bfdf25403fa78568212082ca956
nedasoltanih <neda.sltn_h@yahoo.com>
2025-12-22 18:48:25 +0330

    Models needed for practical session #3

--- reminder/todo/models.py
+++ reminder/todo/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     due_date = models.DateField()          # تاریخ سررسید
     due_time = models.TimeField()          # ساعت سررسید
     done = models.BooleanField(default=False)  # انجام شده یا نه
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,null=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT,null=True)
 
     def __str__(self):
         return self.title
